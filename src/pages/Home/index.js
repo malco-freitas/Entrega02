c1de377a64579b1d5ecc600e45a552b99f764d0b
MalcoEletronics <55303522+MalcoEletronics@users.noreply.github.com>
2020-07-16 23:39:16 -0300

    Add files via upload

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './styles.css';
+import { Buttons } from './styles.js';
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <div className="menuSuperior">
+          <img className="instagram" src="../../components/instagram.png" />
+        </div>
+      </>
+    );
+  }
+}
+
+export default Home;
